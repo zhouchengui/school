8640a2a465bbc1af4e9e061cb77edebaef91a10d
zhouchengui <136461806+zhouchengui@users.noreply.github.com>
2023-06-14 00:27:55 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
+module.exports = {
+  publicPath:'./'
+},
 export default defineConfig({
   plugins: [
     vue(), 
